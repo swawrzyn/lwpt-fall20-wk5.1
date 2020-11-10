e9b72ee881e182f542fcc95f5e14d9fd8c1b7638
Stefan Wawrzyn <swawrzyn@gmail.com>
2020-11-10 21:48:19 +0800

    finished livecode.

--- pages/index/detail.js
+++ pages/index/detail.js
@@ -4,37 +4,65 @@ const app = getApp();
 
 Page({
   data: {
-    items: [
-      { id: 1, text: "This is the first comment." },
-      { id: 2, text: "This is the second comment." },
-      { id: 3, text: "This is the third comment." },
-    ],
-    scrollInto: '',
-    inputVal: '',
+    movie: {
+      title: "Loading...",
+    },
+    items: [{ id: 1, text: "Loading..." }],
+    scrollInto: "",
+    inputVal: "",
   },
-  onLoad: function () {},
-  inputChange: function(e) {
+  onLoad: function (options) {
+    const Movies = new wx.BaaS.TableObject("movies");
+    const MovieReviews = new wx.BaaS.TableObject("movie_reviews");
+
+    // console.log("detail page options", options);
+
+    // Getting a single movie by id (options.id)
+    Movies.get(options.id).then((res) => {
+      // console.log("detail page result", res);
+      this.setData({
+        movie: res.data,
+      });
+    });
+
+    // Getting all the reviews with movie_id == options.id
+    // set up the query
+    let query = new wx.BaaS.Query();
+
+    query.compare("movieId", "=", options.id);
+
+    // grab the information from movie_reviews table
+    MovieReviews.setQuery(query)
+      .find()
+      .then((res) => {
+        console.log("result from movie reviews query find", res);
+        this.setData({
+          items: res.data.objects,
+        });
+      });
+  },
+  inputChange: function (e) {
     this.setData({
       inputVal: e.detail.value,
-    })
+    });
   },
-  formReset: function() {
+  formReset: function () {
     const val = this.data.inputVal;
 
-    if (val.trim() === '') return
+    if (val.trim() === "") return;
     const items = this.data.items;
     const nextId = items.length + 1;
-    items.push({id: nextId, name: val});
+    items.push({ id: nextId, name: val });
     this.setData({
       items,
-    })
+    });
     setTimeout(() => {
       this.setData({
-        scrollInto: `item-${nextId}`
-      })
+        scrollInto: `item-${nextId}`,
+      });
     }, 250);
     this.setData({
-      inputVal: '',
+      inputVal: "",
     });
-  }
+  },
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,45 +1,58 @@
-//index.js
-//获取应用实例
 const app = getApp();
 
 Page({
   data: {
-    items: [
-      { id: 1, name: "Indiana Jones" },
-      { id: 2, name: "Star Wars" },
-      { id: 3, name: "Inception" },
-    ],
-    scrollInto: '',
-    inputVal: '',
+    items: [],
+    scrollInto: "",
+    inputVal: "",
   },
-  onLoad: function () {},
-  toMovie: function(e) {
+  onLoad: function () {
+    const Movies = new wx.BaaS.TableObject("movies");
+
+    // Movies.find() returns a Promise
+    Movies.find().then((result) => {
+      // This is when the promise is RESOLVED (everything ok)
+      console.log("this will happen second.");
+      console.log("result from ifanr", result);
+      this.setData({
+        items: result.data.objects,
+      });
+    }, (error) => {
+      // This is when the promise is REJECTED (something wrong happened)
+      console.log("it's an error!!", error);
+    });
+
+    console.log("This will happen first!");
+  },
+  toMovie: function (e) {
+    console.log('after movie click', e);
     wx.navigateTo({
-      url: 'detail'
-    })
+      // detail?id=fii23fi09r29038r3r290
+      url: `detail?id=${e.currentTarget.id}`,
+    });
   },
-  inputChange: function(e) {
+  inputChange: function (e) {
     this.setData({
       inputVal: e.detail.value,
-    })
+    });
   },
-  formReset: function() {
+  formReset: function () {
     const val = this.data.inputVal;
 
-    if (val.trim() === '') return
+    if (val.trim() === "") return;
     const items = this.data.items;
     const nextId = items.length + 1;
-    items.push({id: nextId, name: val});
+    items.push({ id: nextId, name: val });
     this.setData({
       items,
-    })
+    });
     setTimeout(() => {
       this.setData({
-        scrollInto: `item-${nextId}`
-      })
+        scrollInto: `item-${nextId}`,
+      });
     }, 250);
     this.setData({
-      inputVal: '',
+      inputVal: "",
     });
-  }
+  },
 });
